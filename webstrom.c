80063a5c8af1cf5c49ccc4c0a1cdc3b3ad020dd0
Lucifer20130401 <jingui_w@qq.com>
2019-04-11 15:14:57 +0800

    webstrom SW download addr

---
+++ webstrom.c
@@ -0,0 +1,19 @@
+
+1.http://pan.baidu.com/s/1kV5OWcf  密码：azvf
+
+2.下载后解压，点击exe文件安装
+
+3.将resources_cn.jar文件拷贝到安装后文件夹下的lib文件夹下
+
+4.运行桌面快捷程序，注册时选择“license server” 输入 "http://idea.imsxm.com/"或者“http://idea.iteblog.com/key.php”，点击Activate激活就行
+
+
+---------------------
+
+作者：蜗牛ha
+
+来源：CSDN
+
+原文：https://blog.csdn.net/tan9374/article/details/77711925
+
+
