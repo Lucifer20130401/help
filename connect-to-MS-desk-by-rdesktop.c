0bf0b949e279a44f41408894e356ed3d8ac0e7c7
Lucifer20130401 <jingui_w@qq.com>
2019-05-08 10:52:45 +0800

    rdesktop

---
+++ connect-to-MS-desk-by-rdesktop.c
@@ -0,0 +1,13 @@
+this is to show how to connect to the MS windows desktop by rdesktop
+
+1. sudo apt-grt install rdesktop
+2. set MS window and the permission
+3. rdesktop -f -a 16 -u rdc -p rdc168 192.168.99.44
+
+	-f  full sreen mode
+	-a  set color
+	-u  user name
+	-p  password
+
+use Crtl+Alt+Enter to switch max screen mode and the full screen mode.	
+
